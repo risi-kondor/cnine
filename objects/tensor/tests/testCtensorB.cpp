a81dd114113fddf858f237275b3d104ca8e75135
Risi Kondor <risi@cs.uchicago.edu>
2022-01-19 11:51:52 -0600

    'changes'

---
+++ objects/tensor/tests/testCtensorB.cpp
@@ -0,0 +1,68 @@
+//  This file is part of cnine, a lightweight C++ tensor library. 
+// 
+//  Copyright (c) 2022, Imre Risi Kondor
+//
+//  This Source Code Form is subject to the terms of the Mozilla
+//  Public License v. 2.0. If a copy of the MPL was not distributed
+//  with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+#include "Cnine_base.cpp"
+#include "CtensorB.hpp"
+#include "CnineSession.hpp"
+
+using namespace cnine;
+
+typedef CtensorB ctensor; 
+
+
+int main(int argc, char** argv){
+
+  cnine_session session;
+
+  cout<<endl;
+
+  ctensor A=ctensor::sequential({4,4});
+  cout<<"A="<<endl<<A<<endl<<endl;;
+
+  ctensor B=ctensor::gaussian({4,4});
+  cout<<"B="<<endl<<B<<endl<<endl;
+
+  /*
+  cscalar c(2.0);
+
+  cout<<"A+B="<<endl<<A+B<<endl<<endl;
+  cout<<"A-B="<<endl<<A-B<<endl<<endl;
+  cout<<"c*A="<<endl<<c*A<<endl<<endl;
+  cout<<"A*B="<<endl<<A*B<<endl<<endl;
+  cout<<endl; 
+
+  cout<<"norm2(A)="<<endl<<norm2(A)<<endl<<endl;
+  cout<<"inp(A,B)="<<endl<<inp(A,B)<<endl<<endl;
+  cout<<"ReLU(A,0.1)="<<endl<<ReLU(A,0.1)<<endl<<endl;
+  cout<<endl; 
+
+  ctensor C=ctensor::zero({4,4});
+  C+=A;
+  C+=A;
+  C+=A;
+  C+=A;
+  cout<<"A+A+A+A="<<endl<<C<<endl;
+  cout<<endl; 
+
+  ctensor N=A.col_norms();
+  cout<<N<<endl; 
+
+  ctensor D=A.divide_cols(N);
+  cout<<D<<endl;
+  cout<<D.col_norms()<<endl; 
+
+  cout<<"  fn(C) = "<<endl<<C.apply([](const complex<float> x){return x*x+complex<float>(3.0);})<<endl; 
+  cout<<"  fn2(C) = "<<endl<<C.apply([](const int i, const int j, const complex<float> x){return x+complex<float>(i,j);})<<endl; 
+
+  cout<<ctensor(Gdims({5,5}),[](const int i, const int j){return complex<float>(i+j);})<<endl;
+
+  cout<<endl;
+  */
+
+}
---
+++ objects/tensor/tests/testSet.cpp
@@ -0,0 +1,33 @@
+//  This file is part of cnine, a lightweight C++ tensor library. 
+// 
+//  Copyright (c) 2022, Imre Risi Kondor
+//
+//  This Source Code Form is subject to the terms of the Mozilla
+//  Public License v. 2.0. If a copy of the MPL was not distributed
+//  with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+#include "Cnine_base.cpp"
+//#include "CtensorB.hpp"
+#include "CnineSession.hpp"
+
+using namespace cnine;
+
+
+int main(int argc, char** argv){
+
+  cnine_session session;
+
+
+  set<int> A={1,3,2};
+  set<int> B={2,1,2,3,4};
+
+  for(auto p: A)
+    cout<<p<<endl; 
+
+  for(auto p: B)
+    cout<<p<<endl; 
+
+  cout<<(A==B)<<endl;
+
+}
