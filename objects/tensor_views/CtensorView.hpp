5f380301a99ba1ee7c156099c846a75aabadc3e2
Risi Kondor <risi@cs.uchicago.edu>
2022-05-26 23:26:28 -0500

    'changes'

---
+++ objects/tensor_views/CtensorView.hpp
@@ -0,0 +1,203 @@
+//  This file is part of cnine, a lightweight C++ tensor library. 
+// 
+//  Copyright (c) 2021, Imre Risi Kondor
+//
+//  This Source Code Form is subject to the terms of the Mozilla
+//  Public License v. 2.0. If a copy of the MPL was not distributed
+//  with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+#ifndef _CnineCtensorView
+#define _CnineCtensorView
+
+#include "Cnine_base.hpp"
+#include "Gstrides.hpp"
+#include "Gtensor.hpp"
+
+#include "Ctensor3_view.hpp"
+
+
+namespace cnine{
+
+
+  class CtensorView{
+  public:
+
+    float* arr;
+    float* arrc;
+    const Gdims dims;
+    const Gstrides strides;
+    int dev=0;
+
+  public:
+
+    CtensorView(){}
+
+    CtensorView(float* _arr, float* _arrc): 
+      arr(_arr), arrc(_arrc){}
+
+    CtensorView(float* _arr, float* _arrc, const int _n0, const int _s0, const int _dev=0): 
+      arr(_arr), arrc(_arrc), dims(_n0), strides(_s0), dev(_dev){}
+
+    CtensorView(float* _arr, float* _arrc, const int _n0, const int _n1, const int _s0, const int _s1, const int _dev=0): 
+      arr(_arr), arrc(_arrc), dims(_n0,_n1), strides(_s0,_s1), dev(_dev){}
+
+    CtensorView(float* _arr, float* _arrc, const int _n0, const int _n1, const int _n2, 
+      const int _s0, const int _s1, const int _s2, const int _dev=0): 
+      arr(_arr), arrc(_arrc), dims(_n0,_n1,_n2), strides(_s0,_s1,_s2), dev(_dev){}
+
+    CtensorView(float* _arr, float* _arrc, const int _n0, const int _n1, const int _n2, const int _n3,
+      const int _s0, const int _s1, const int _s2, const int _s3, const int _dev=0): 
+      arr(_arr), arrc(_arrc), dims(_n0,_n1,_n2,_n3), strides(_s0,_s1,_s2,_s3), dev(_dev){}
+
+    CtensorView(float* _arr,  float* _arrc, const Gdims& _dims, const Gstrides& _strides, const int _dev=0):
+      arr(_arr), arrc(_arrc), dims(_dims), strides(_strides), dev(_dev){
+    }
+
+
+
+  public: // ---- Getters ------------------------------------------------------------------------------------
+
+
+    complex<float> operator()(const int i0) const{
+      CNINE_CHECK_RANGE(if(i0<0 || i0>=dims[0]) 
+	  throw std::out_of_range("cnine::CtensorView: index "+Gindex({i0}).str()+" out of range of view size "+dims.str()));
+      return std::complex<float>(arr[strides.offs(i0)],arrc[strides.offs(i0)]);
+    }
+
+    complex<float> operator()(const int i0, const int i1) const{
+      CNINE_CHECK_RANGE(if(i0<0 || i1<0 || i0>=dims[0] || i1>=dims[1]) 
+	  throw std::out_of_range("cnine::CtensorView: index "+Gindex({i0,i1}).str()+" out of range of view size "+dims.str()));
+      return std::complex<float>(arr[strides.offs(i0,i1)],arrc[strides.offs(i0,i1)]);
+
+    }
+
+    complex<float> operator()(const int i0, const int i1, const int i2) const{
+      CNINE_CHECK_RANGE(if(i0<0 || i1<0 || i2<0 || i0>=dims[0] || i1>=dims[1] || i2>=dims[2]) 
+	  throw std::out_of_range("cnine::CtensorView: index "+Gindex({i0,i1,i2}).str()+" out of range of view size "+dims.str()));
+      return std::complex<float>(arr[strides.offs(i0,i1,i2)],arrc[strides.offs(i0,i1,i2)]);
+
+    }
+
+    complex<float> operator()(const int i0, const int i1, const int i2, const int i3) const{
+      CNINE_CHECK_RANGE(if(i0<0 || i1<0 || i2<0 || i3<0 || i0>=dims[0] || i1>=dims[1] || i2>=dims[2] || i3>=dims[3]) 
+	  throw std::out_of_range("cnine::CtensorView: index "+Gindex({i0,i1,i2,i3}).str()+" out of range of view size "+dims.str()));
+      return std::complex<float>(arr[strides.offs(i0,i1,i2,i3)],arrc[strides.offs(i0,i1,i2,i3)]);
+    }
+
+
+  public: // ---- Setters ------------------------------------------------------------------------------------
+
+
+    void set(const int i0, complex<float> x){
+      CNINE_CHECK_RANGE(if(i0<0 || i0>=dims[0]) 
+	  throw std::out_of_range("cnine::CtensorView: index "+Gindex({i0}).str()+" out of range of view size "+dims.str()));
+      arr[strides.offs(i0)]=std::real(x);
+      arrc[strides.offs(i0)]=std::imag(x);
+    }
+
+    void set(const int i0, const int i1, complex<float> x){
+      CNINE_CHECK_RANGE(if(i0<0 || i1<0 || i0>=dims[0] || i1>=dims[1]) 
+	  throw std::out_of_range("cnine::CtensorView: index "+Gindex({i0,i1}).str()+" out of range of view size "+dims.str()));
+      arr[strides.offs(i0,i1)]=std::real(x);
+      arrc[strides.offs(i0,i1)]=std::imag(x);
+    }
+
+    void set(const int i0, const int i1, const int i2, complex<float> x){
+      CNINE_CHECK_RANGE(if(i0<0 || i1<0 || i2<0 || i0>=dims[0] || i1>=dims[1] || i2>=dims[2]) 
+	  throw std::out_of_range("cnine::CtensorView: index "+Gindex({i0,i1,i2}).str()+" out of range of view size "+dims.str()));
+      arr[strides.offs(i0,i1,i2)]=std::real(x);
+      arrc[strides.offs(i0,i1,i2)]=std::imag(x);
+    }
+
+    void set(const int i0, const int i1, const int i2, const int i3, complex<float> x){
+      CNINE_CHECK_RANGE(if(i0<0 || i1<0 || i2<0 || i3<0 || i0>=dims[0] || i1>=dims[1] || i2>=dims[2] || i3>=dims[3]) 
+	  throw std::out_of_range("cnine::CtensorView: index "+Gindex({i0,i1,i2,i3}).str()+" out of range of view size "+dims.str()));
+      arr[strides.offs(i0,i1,i2)]=std::real(x);
+      arrc[strides.offs(i0,i1,i2)]=std::imag(x);
+    }
+
+
+  public: // ---- Incrementers -------------------------------------------------------------------------------
+
+
+    void inc(const int i0, complex<float> x){
+      CNINE_CHECK_RANGE(if(i0<0 || i0>=dims[0]) 
+	  throw std::out_of_range("cnine::CtensorView: index "+Gindex({i0}).str()+" out of range of view size "+dims.str()));
+      arr[strides.offs(i0)]+=std::real(x);
+      arrc[strides.offs(i0)]+=std::imag(x);
+    }
+
+    void inc(const int i0, const int i1, complex<float> x){
+      CNINE_CHECK_RANGE(if(i0<0 || i1<0 || i0>=dims[0] || i1>=dims[1]) 
+	  throw std::out_of_range("cnine::CtensorView: index "+Gindex({i0,i1}).str()+" out of range of view size "+dims.str()));
+      arr[strides.offs(i0,i1)]+=std::real(x);
+      arrc[strides.offs(i0,i1)]+=std::imag(x);
+    }
+
+    void inc(const int i0, const int i1, const int i2, complex<float> x){
+      CNINE_CHECK_RANGE(if(i0<0 || i1<0 || i2<0 || i0>=dims[0] || i1>=dims[1] || i2>=dims[2]) 
+	  throw std::out_of_range("cnine::CtensorView: index "+Gindex({i0,i1,i2}).str()+" out of range of view size "+dims.str()));
+      arr[strides.offs(i0,i1,i2)]+=std::real(x);
+      arrc[strides.offs(i0,i1,i2)]+=std::imag(x);
+    }
+
+    void inc(const int i0, const int i1, const int i2, const int i3, complex<float> x){
+      CNINE_CHECK_RANGE(if(i0<0 || i1<0 || i2<0 || i3<0 || i0>=dims[0] || i1>=dims[1] || i2>=dims[2] || i3>=dims[3]) 
+	  throw std::out_of_range("cnine::CtensorView: index "+Gindex({i0,i1,i2,i3}).str()+" out of range of view size "+dims.str()));
+      arr[strides.offs(i0,i1,i2,i3)]+=std::real(x);
+      arrc[strides.offs(i0,i1,i2,i3)]+=std::imag(x);
+    }
+
+
+
+
+    //CtensorView block(const int i0, const int i1, const int i2, const int i3, const int m0, const int m1, const int m2, const int m3) const{
+    //return CtensorView(arr+i0*s0+i1*s1+i2*s2+i3*s3,m0,m1,m2,m3,s0,s1,s2,s3,dev);
+    //}
+
+
+  public: // ---- Cumulative operations ---------------------------------------------------------------------
+
+
+
+  public: // ---- Other views -------------------------------------------------------------------------------
+
+
+
+  public: // ---- Conversions -------------------------------------------------------------------------------
+
+
+    /*
+    Gtensor<float> gtensor() const{
+      Gtensor<float> R({n0,n1,n2,n3},fill::raw);
+      for(int i0=0; i0<n0; i0++)
+	for(int i1=0; i1<n1; i1++)
+	  for(int i2=0; i2<n2; i2++)
+	    for(int i3=0; i3<n3; i3++)
+	      R(i0,i1,i2,i3)=(*this)(i0,i1,i2,i3);
+      return R;
+    }
+    */
+    
+    
+  public: // ---- I/O ----------------------------------------------------------------------------------------
+
+  
+    string str(const string indent="") const{
+      return ("");
+      //return gtensor().str(indent);
+    }
+
+    friend ostream& operator<<(ostream& stream, const CtensorView& x){
+      stream<<x.str(); return stream;
+    }
+
+
+  };
+
+
+}
+
+
+#endif 
