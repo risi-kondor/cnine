e44211624216542dcbaa207b46a4644ee191fb1f
Risi Kondor <risi@cs.uchicago.edu>
2022-06-07 00:34:36 -0500

    'changes'

---
+++ objects/tensor_array/tests/test_CtensorB_array.cpp
@@ -0,0 +1,34 @@
+#include "Cnine_base.cpp"
+#include "CnineSession.hpp"
+#include "CtensorB_array.hpp"
+#include "RtensorA.hpp"
+
+using namespace cnine;
+
+
+int main(int argc, char** argv){
+  cnine_session session;
+  cout<<endl;
+
+  int n=4;
+  Gdims adims({n});
+  Gdims cdims({2,2});
+
+  CtensorB_array u=CtensorB_array::gaussian(adims,cdims);
+  PRINTL(u);
+
+  RtensorA M=RtensorA::zero({n,n});
+  M.set(0,1,1.0);
+  M.set(0,3,1.0);
+  M.set(2,2,1.0);
+  cout<<M<<endl;
+
+  Rmask1 mask=Rmask1::matrix(M.view2());
+  cout<<mask<<endl;
+  cout<<mask.inv()<<endl;
+  
+  auto w=u.gather(mask);
+  PRINTL(w);
+}
+
+
