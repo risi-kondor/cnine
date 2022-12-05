654ceb357432542e51b4191dc48892f317849725
Risi Kondor <risi@cs.uchicago.edu>
2022-12-05 16:17:54 -0600

    'changes'

--- cuda/RtensorConvolve3d.cu
+++ cuda/RtensorConvolve3d.cu
@@ -130,9 +130,10 @@ __global__ void RtensorConvolve3d_kernel
 (float* rarr, const int rs0, const int rs1, const int rs2, const int rs3, const int rs4, const int rs5,   
   float* xarr, const int xs0, const int xs1, const int xs2, const int xs3, const int xs4, const int xs5, 
   float* warr, const int ws0, const int ws1, const int ws2, const int ws3, const int ws4, 
-  const int nj0, const int nj1, const int nj2, const int na){
+  const int rn0, const int nj0, const int nj1, const int nj2, const int na){
 
-  int i0=blockIdx.x;
+  int b=blockIdx.x/rn0;
+  int i0=blockIdx.x%rn0;
   int i1=blockIdx.y;
   int i2=blockIdx.z;
 
@@ -141,10 +142,10 @@ __global__ void RtensorConvolve3d_kernel
     for(int j1=0; j1<nj1; j1++)
       for(int j2=0; j2<nj2; j2++)
 	for(int a=0; a<na; a++)
-	  t+=xarr[(i0+j0)*xs0+(i1+j1)*xs1+(i2+j2)*xs2+a*xs3+threadIdx.y*xs4]*
+	  t+=xarr[b*xs0+(i0+j0)*xs1+(i1+j1)*xs2+(i2+j2)*xs3+a*xs4+threadIdx.y*xs5]*
 	    warr[threadIdx.x*ws0+j0*ws1+j1*ws2+j2*ws3+a*ws4];
 
-  rarr[i0*rs0+i1*rs1+i2*rs2+threadIdx.x*rs3+threadIdx.y*rs4]+=t;
+  rarr[b*rs0+i0*rs1+i1*rs2+i2*rs3+threadIdx.x*rs4+threadIdx.y*rs5]+=t;
 }
 
 
@@ -152,10 +153,11 @@ __global__ void RtensorConvolve3d_kernel
 (float* rarr, const int rs0, const int rs1, const int rs2, const int rs3, const int rs4, const int rs5,   
   float* xarr, const int xs0, const int xs1, const int xs2, const int xs3, const int xs4, const int xs5, 
   float* warr, const int ws0, const int ws1, const int ws2, const int ws3, const int ws4, 
-  const int nj0, const int nj1, const int nj2, const int na,
+  const int rn0, const int nj0, const int nj1, const int nj2, const int na,
   const int xn0, const int xn1, const int xn2, const int padding0, const int padding1, const int padding2){
 
-  int i0=blockIdx.x;
+  int b=blockIdx.x/rn0;
+  int i0=blockIdx.x%rn0;
   int i1=blockIdx.y;
   int i2=blockIdx.z;
 
@@ -164,10 +166,10 @@ __global__ void RtensorConvolve3d_kernel
     for(int j1=max(1,padding1-i1); j1<min(nj1,xn1-i1+padding1); j1++)
       for(int j2=max(2,padding2-i2); j2<min(nj2,xn2-i2+padding2); j2++)
 	for(int a=0; a<na; a++)
-	  t+=xarr[(i0+j0)*xs0+(i1+j1)*xs1+(i2+j2)*xs2+a*xs3+threadIdx.y*xs4]*
+	  t+=xarr[b*xs0+(i0+j0)*xs1+(i1+j1)*xs2+(i2+j2)*xs3+a*xs4+threadIdx.y*xs5]*
 	    warr[threadIdx.x*ws0+j0*ws1+j1*ws2+j2*ws3+a*ws4];
 
-  rarr[i0*rs0+i1*rs1+i2*rs2+threadIdx.x*rs3+threadIdx.y*rs4]+=t;
+  rarr[b*rs0+i0*rs1+i1*rs2+i2*rs3+threadIdx.x*rs4+threadIdx.y*rs5]+=t;
 }
 
 
@@ -309,21 +311,21 @@ namespace cnine{
     CNINE_ASSRT(w.dev==1);
 
     if(r.n3*r.n4<=1024){
-      dim3 blocks(r.n0,r.n1,r.n2);
-      dim3 threads(r.n3,r.n4);
+      dim3 blocks(r.n0*r.n1,r.n2,r.n3);
+      dim3 threads(r.n4,r.n5);
 
       if(padding0==0&&padding1==0&&padding2==0){
 	RtensorConvolve3d_kernel<<<blocks,threads,0,stream>>>
 	  (r.arr,r.s0,r.s1,r.s2,r.s3,r.s4,r.s5,
 	    x.arr,x.s0,x.s1,x.s2,x.s3,x.s4,x.s5,
 	    w.arr,w.s0,w.s1,w.s2,w.s3,w.s4,
-	    w.n1,w.n2,w.n3,w.n4); 
+	    r.n0,w.n1,w.n2,w.n3,w.n4); 
       }else{
 	RtensorConvolve3d_kernel<<<blocks,threads,0,stream>>>
 	  (r.arr,r.s0,r.s1,r.s2,r.s3,r.s4,r.s5,
 	    x.arr,x.s0,x.s1,x.s2,x.s3,x.s4,x.s5,
 	    w.arr,w.s0,w.s1,w.s2,w.s3,w.s4,
-	    w.n1,w.n2,w.n3,w.n4,
+	    r.n0,w.n1,w.n2,w.n3,w.n4,
 	    x.n1,x.n2,x.n3,padding0,padding1,padding2); 
       }
 
