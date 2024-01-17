280a2beb2433cef56b194dbaa1c139ae5142c231
Risi Kondor <risi@cs.uchicago.edu>
2024-01-17 00:32:43 -0600

    'changes'

---
+++ hpc/GatherMapProgramPack.hpp
@@ -0,0 +1,28 @@
+/*
+ * This file is part of cnine, a lightweight C++ tensor library. 
+ *  
+ * Copyright (c) 2021, Imre Risi Kondor
+ *
+ * This source code file is subject to the terms of the noncommercial 
+ * license distributed with cnine in the file LICENSE.TXT. Commercial 
+ * use is prohibited. All redistributed versions of this file (in 
+ * original or modified form) must retain this copyright notice and 
+ * must be accompanied by a verbatim copy of the license. 
+ *
+ */
+
+#ifndef _GatherMapProgramPack
+#define _GatherMapProgramPack
+
+#include "Cnine_base.hpp"
+#include "GatherMapProgram.hpp"
+
+
+namespace cnine{
+
+
+  class GatherMapProgramPack: vector<GatherMapProgram>{
+
+  }
+
+}
