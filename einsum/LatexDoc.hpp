b494be6c6143316bfe8883654f1c3cab62331558
Risi Kondor <risi@cs.uchicago.edu>
2024-09-17 01:23:51 -0500

    'changes'

---
+++ einsum/LatexDoc.hpp
@@ -0,0 +1,50 @@
+/*
+ * This file is part of cnine, a lightweight C++ tensor library. 
+ *  
+ * Copyright (c) 2021, Imre Risi Kondor
+ *
+ * This source code file is subject to the terms of the noncommercial 
+ * license distributed with cnine in the file LICENSE.TXT. Commercial 
+ * use is prohibited. All redistributed versions of this file (in 
+ * original or modified form) must retain this copyright notice and 
+ * must be accompanied by a verbatim copy of the license. 
+ *
+ */
+
+
+#ifndef _LatexDoc
+#define _LatexDoc
+
+#include "TensorView.hpp"
+#include "EinsumFormN.hpp"
+#include "EinsumPrograms.hpp"
+
+namespace cnine{
+
+
+  class LatexDoc: public string{
+  public:
+
+    LatexDoc(){
+      (*this)+="\\documentclass{article}\n";
+      (*this)+="\\begin{document}\n";
+    }
+
+    LatexDoc(const string str):
+      LatexDoc(){
+      (*this)+=str;
+      finish();
+    }
+
+    void finish(){
+      (*this)+="\n \\end{document}\n";
+    }
+
+    
+
+
+  };
+
+}
+
+#endif 
