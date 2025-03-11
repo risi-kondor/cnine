5b20a038b1ab374cb4e525f16e8cdf14b7a8555c
Risi Kondor <risi@cs.uchicago.edu>
2025-03-10 22:07:55 -0500

    'changes'

--- python/bindings/cnine_py.cpp
+++ python/bindings/cnine_py.cpp
@@ -20,35 +20,35 @@
 #include "Gdims.hpp"
 #include "Gindex.hpp"
 
-#include "RscalarObj.hpp"
-#include "RtensorObj.hpp"
-#include "RtensorArray.hpp"
+//#include "RscalarObj.hpp"
+//#include "RtensorObj.hpp"
+//#include "RtensorArray.hpp"
 
-#include "CscalarObj.hpp"
-#include "CtensorObj.hpp"
-#include "CtensorPackObj.hpp"
-#include "CtensorArray.hpp"
+//#include "CscalarObj.hpp"
+//#include "CtensorObj.hpp"
+//#include "CtensorPackObj.hpp"
+//#include "CtensorArray.hpp"
 
-#include "CtensorB.hpp"
-#include "Aggregator.hpp"
+//#include "CtensorB.hpp"
+//#include "Aggregator.hpp"
 
-#include "CSRmatrix.hpp"
+//#include "CSRmatrix.hpp"
 
 //std::default_random_engine rndGen;
 
-#include "GenericCop.hpp"
-#include "RtensorA_add_plus_cop.hpp"
-#include "CtensorA_add_plus_cop.hpp"
+//#include "GenericCop.hpp"
+//#include "RtensorA_add_plus_cop.hpp"
+//#include "CtensorA_add_plus_cop.hpp"
 
-#include "CellwiseBinaryCmap.hpp"
+//#include "CellwiseBinaryCmap.hpp"
 
-#include "Tensor.hpp"
-#include "TensorPack.hpp"
+//#include "Tensor.hpp"
+//#include "TensorPack.hpp"
 
 //  using namespace cnine;
 //namespace py=pybind11;
 
-#include "cmaps_py.cpp"
+//#include "cmaps_py.cpp"
 
 #include "CnineSession.hpp"
 cnine::cnine_session session;
@@ -77,7 +77,7 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     .def(pybind11::init<vector<int> >())
     .def(pybind11::init<vector<int> >(),"Initialize a Gdims  object from a list of integers.")
 
-    .def("__len__",&Gdims::k)
+    .def("__len__",&Gdims::size)
     .def("__getitem__",&Gdims::operator())
     .def("__setitem__",&Gdims::set)
 
@@ -100,34 +100,35 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
 
 
 
-  pybind11::class_<RscalarObj>(m,"rscalar")
-    .def(pybind11::init<>())
-    .def("str",&RscalarObj::str,py::arg("indent")="")
-    .def("__str__",&RscalarObj::str,py::arg("indent")="");
+  //  pybind11::class_<RscalarObj>(m,"rscalar")
+  //  .def(pybind11::init<>())
+  //  .def("str",&RscalarObj::str,py::arg("indent")="")
+  //  .def("__str__",&RscalarObj::str,py::arg("indent")="");
 
 
 
-  pybind11::class_<CscalarObj>(m,"cscalar")
-    .def(pybind11::init<>())
-    .def("str",&CscalarObj::str,py::arg("indent")="")
-    .def("__str__",&CscalarObj::str,py::arg("indent")="");
+  //pybind11::class_<CscalarObj>(m,"cscalar")
+  //  .def(pybind11::init<>())
+  //  .def("str",&CscalarObj::str,py::arg("indent")="")
+  //  .def("__str__",&CscalarObj::str,py::arg("indent")="");
   
 
-#include "rtensor_py.cpp"
-#include "ctensor_py.cpp"
-#include "tensor_py.cpp"
-#include "tensorpack_py.cpp"
+  //#include "rtensor_py.cpp"
+  //#include "ctensor_py.cpp"
+  //#include "tensor_py.cpp"
+  //#include "tensorpack_py.cpp"
 
-#include "ctensorpack_py.cpp"
+    //#include "ctensorpack_py.cpp"
   
-#include "rtensorarr_py.cpp"
-#include "ctensorarr_py.cpp"
+    //#include "rtensorarr_py.cpp"
+  //#include "ctensorarr_py.cpp"
 
-#include "ctensorb_py.cpp"
+    //#include "ctensorb_py.cpp"
 
-#include "CSRmatrix_py.cpp"
+    //#include "tensor_py.cpp"
+  //#include "CSRmatrix_py.cpp"
 
-#include "Rmask1_py.cpp"
+    //#include "Rmask1_py.cpp"
 
   //#include "cmaps_py.cpp"
     
