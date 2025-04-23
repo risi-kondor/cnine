b8b5ae6ca80b5341560bccf3183a8e94c34e85c1
Risi Kondor <risi@cs.uchicago.edu>
2025-04-23 10:14:48 -0500

    'changes'

--- hpc/tests/testGatherSlices.cpp
+++ hpc/tests/testGatherSlices.cpp
@@ -18,7 +18,20 @@ int main(int argc, char** argv){
   TensorView<float> Y(dims(5,3),0,0);
 
   GatherSlices()(Y,X,g,0);
-
   cout<<Y<<endl;
+
+  auto Z=GatherSlices()(X,g,0);
+  cout<<Z<<endl;
+
+  auto W=GatherSlices().naive(X,g,0);
+  cout<<W<<endl;
+
+
+  TensorView<float> A(dims(3,5,4),4,0);
+  auto B=GatherSlices()(A,g,1);
+  cout<<B<<endl;
+  auto C=GatherSlices().naive(A,g,1);
+  cout<<C<<endl;
+
 
 }
